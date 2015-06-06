ff7decc68e67322c9f219fb5cf4327ebc79f12c3
Carlos Hinojosa <cahinojos@gmail.com>
2015-06-06 20:59:41 +0000

    v01

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,20 +1,32 @@
 (function () {
-    document.addEventListener("deviceready", function () {
+        document.addEventListener("deviceready", function () {
 
-        window.listview = kendo.observable({
-            addImage: function () {
-                $("#images")
-                    .data("kendoMobileListView")
-                    .prepend(["images/08.jpg"]);
-            }
-        });
-        var app = new kendo.mobile.Application(document.body, {
-            skin: "flat"
-        });
-        $("#images").kendoMobileListView({
-            dataSource: ["images/01.jpg", "images/02.jpg", "images/03.jpg", "images/04.jpg", "images/05.jpg", "images/06.jpg", "images/07.jpg"],
-            template: "<img src='#: data #'>"
-        });
-        navigator.splashscreen.hide();
-    });
-}());
+                window.listview = kendo.observable({
+                        addImage: function () {
+                            var success = function (data) {
+                                $("#images")
+                                    .data("kendoMobileListView")
+                                    .prepend(["data:image/jpeg;base64," + data]);
+                            };
+
+                            var error = function () {
+                                navigator.notification.alert("Unfortunately we could not add the image");
+                            };
+                            var config = {
+                                destinationType: Camera.DestinationType.DATA_URL,
+                                targetHeight: 400,
+                                targetWidth: 400
+                            };
+                            navigator.camera.getPicture(success, error, config);
+
+
+                        });
+                    
+                    var app = new kendo.mobile.Application(document.body, {
+                        skin: "flat"
+                    }); $("#images").kendoMobileListView({
+                        dataSource: ["images/01.jpg", "images/02.jpg", "images/03.jpg", "images/04.jpg", "images/05.jpg", "images/06.jpg", "images/07.jpg"],
+                        template: "<img src='#: data #'>"
+                    }); navigator.splashscreen.hide();
+                });
+        })});
